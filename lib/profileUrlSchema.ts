e7b12677331d2f37f42890ab4c007a5e50eaaab0
Felix Sebastian <hi@felixsebastian.dev>
2024-06-24 23:35:06 +0700

    fix validation message

--- lib/profileUrlSchema.ts
+++ lib/profileUrlSchema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 import { steamIdRegex, vanityUrlRegex } from "./parseSteamProfileUrl";
 
 const profileUrlSchema = z.union([
-  z.string().regex(steamIdRegex),
-  z.string().regex(vanityUrlRegex),
+  z.string().regex(steamIdRegex, { message: "Must be a valid steam URL" }),
+  z.string().regex(vanityUrlRegex, { message: "Must be a valid steam URL" }),
 ]);
 
 export default profileUrlSchema;
