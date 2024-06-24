a9a2795d0435c0537a681747509c542d4261c98a
Felix Sebastian <hi@felixsebastian.dev>
2024-06-24 16:00:42 +0700

    rounded thumbnail

--- components/SteamThumbnail.tsx
+++ components/SteamThumbnail.tsx
@@ -2,12 +2,14 @@ import { OwnedGame } from "@/lib/types";
 import Image from "next/image";
 
 const SteamThumbnail = (props: OwnedGame) => (
-  <Image
-    alt={props.name}
-    src={`http://media.steampowered.com/steamcommunity/public/images/apps/${props.appid}/${props.img_icon_url}.jpg`}
-    width={30}
-    height={30}
-  />
+  <div className="rounded overflow-hidden">
+    <Image
+      alt={props.name}
+      src={`http://media.steampowered.com/steamcommunity/public/images/apps/${props.appid}/${props.img_icon_url}.jpg`}
+      width={24}
+      height={24}
+    />
+  </div>
 );
 
 export default SteamThumbnail;
