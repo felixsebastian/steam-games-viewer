7e5f69a3e25b2ab76d6ebbdcada594669793238f
Felix Sebastian <hi@felixsebastian.dev>
2024-06-24 22:02:51 +0700

    cleanup test

--- tests/games.test.ts
+++ tests/games.test.ts
@@ -4,43 +4,35 @@
 import { GET } from "@/app/api/games/route";
 import { NextRequest } from "next/server";
 import mockSteamResponse from "./fixtures/mockSteamResponse";
-import "../lib/SteamApiClient";
 
 jest.mock("../lib/SteamApiClient", () => {
   return {
-    __esModule: true,
-    default: jest.fn().mockImplementation(() => {
-      return {
-        fetchGames: () => Promise.resolve(mockSteamResponse),
-      };
+    SteamApiClient: jest.fn().mockImplementation(() => {
+      return { fetchGames: () => Promise.resolve(mockSteamResponse) };
     }),
   };
 });
 
 describe("/api/games", () => {
   describe("invalid steamid", () => {
-    test("returns a 4XX error", () => {
+    test("returns a 4XX error", async () => {
       const req = {
         nextUrl: { search: "steamid=abc" },
       } as NextRequest;
 
-      (async () => {
-        const response = await GET(req);
-        expect(response.status).toBe(400);
-      })();
+      const response = await GET(req);
+      expect(response.status).toBe(400);
     });
   });
 
   describe("valid steamid", () => {
-    test("returns the data", () => {
+    test("returns the data", async () => {
       const req = {
         nextUrl: { search: "steamid=76561197960434622" },
       } as NextRequest;
 
-      (async () => {
-        const response = await GET(req);
-        expect(response.status).toBe(200);
-      })();
+      const response = await GET(req);
+      expect(response.status).toBe(200);
     });
   });
 });
