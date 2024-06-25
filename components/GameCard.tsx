59e8cafa52d8810557db6ea04a1366187c18429c
Felix Sebastian <hi@felixsebastian.dev>
2024-06-25 15:38:08 +0700

    clickable cards

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -6,43 +6,46 @@ import { TrophyIcon } from "lucide-react";
 import { OwnedGame } from "@/lib/types";
 import { Tooltip, TooltipTrigger } from "@radix-ui/react-tooltip";
 import { TooltipContent } from "./ui/tooltip";
+import Link from "next/link";
 
 interface Props {
   game: OwnedGame;
 }
 
 const GameCard = ({ game }: Props) => (
-  <Card>
-    <CardHeader>
-      <div className="flex flex-row justify-between items-start">
-        <div className="flex flex-col items-start gap-2">
-          <CardTitle>
-            <Tooltip>
-              <TooltipTrigger>
-                <div className="text-left w-min max-w-80 block overflow-x-hidden leading-8 whitespace-nowrap text-ellipsis">
-                  {game.name}
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{game.name}</p>
-              </TooltipContent>
-            </Tooltip>
-          </CardTitle>
-          <div className="flex gap-2 flex-wrap">
-            <Playtime minutes={game.playtime_forever} />
-            {game.most_played && (
-              <Badge variant="outline">
-                <span className="flex gap-1 items-center">
-                  <TrophyIcon size={12} /> Most played
-                </span>
-              </Badge>
-            )}
+  <Link href={`https://steamcommunity.com/app/${game.appid}`} target="_blank">
+    <Card>
+      <CardHeader>
+        <div className="flex flex-row justify-between items-start">
+          <div className="flex flex-col items-start gap-2">
+            <CardTitle>
+              <Tooltip>
+                <TooltipTrigger>
+                  <div className="text-left w-min max-w-64 lg:max-w-80 block overflow-x-hidden leading-8 whitespace-nowrap text-ellipsis">
+                    {game.name}
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{game.name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </CardTitle>
+            <div className="flex gap-2 flex-wrap">
+              <Playtime minutes={game.playtime_forever} />
+              {game.most_played && (
+                <Badge variant="outline">
+                  <span className="flex gap-1 items-center">
+                    <TrophyIcon size={12} /> Most played
+                  </span>
+                </Badge>
+              )}
+            </div>
           </div>
+          <SteamThumbnail {...game} />
         </div>
-        <SteamThumbnail {...game} />
-      </div>
-    </CardHeader>
-  </Card>
+      </CardHeader>
+    </Card>
+  </Link>
 );
 
 export default GameCard;
